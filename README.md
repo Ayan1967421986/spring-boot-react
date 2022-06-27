ae5d62e03615ed7a0b1cda14a02530eb017a3191
Ayan1967421986 <Ayan.Dutta.1986@gmail.com>
2022-06-27 22:05:25 +0100

    AD-100123:Sprint-Boot Rest Application with React UI : Working Solution

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 
 - [mvn spring-boot:run]
 - React application will be fully integrated into our API at the http://localhost:8080/ URL
+
+![](/Users/ayan.dutta.1986/Coding/WorkSpace/spring-boot-rest-react/screens/ViewCard.png)
+![](/Users/ayan.dutta.1986/Coding/WorkSpace/spring-boot-rest-react/screens/AddCard.png)
--- target/generated-sources/annotations/com/ad/springbootreact/mapper/CardMapperImpl.java
+++ target/generated-sources/annotations/com/ad/springbootreact/mapper/CardMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-06-27T21:42:09+0100",
+    date = "2022-06-27T21:55:20+0100",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 18.0.1.1 (Oracle Corporation)"
 )
 @Component
